7428cb7de2da8cee4c5e0537dffb5046fc67ea8e
Muhammad Hammad Yousaf <hammad.yousaf@arbisoft.com>
2025-02-27 19:20:53 +0500

    add form improvements

--- edly_selfhosted/edly_selfhosted/models.py
+++ edly_selfhosted/edly_selfhosted/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class OnboardingSurvey(models.Model):
+    # Indicates whether the user has completed the onboarding form
     form_filled = models.BooleanField(default=False)
+    
+    # Flag to control whether the form should be automatically loaded on the frontend
+    auto_load_form = models.BooleanField(default=True)
--- edly_selfhosted/edly_selfhosted/serializer.py
+++ edly_selfhosted/edly_selfhosted/serializer.py
@@ -3,4 +3,5 @@ from rest_framework import serializers
 
 class OnboardingSurveySerializers(serializers.Serializer):
     form_filled = serializers.BooleanField()
+    auto_load_form = serializers.BooleanField()
 
